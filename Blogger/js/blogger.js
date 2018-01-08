f1ee6be29a1ead45a4544d90b673092edba400ee
piyush <piyush2k13@gmail.com>
2018-01-08 21:57:16 +0530

    added more contents

--- Blogger/js/blogger.js
+++ Blogger/js/blogger.js
@@ -6,6 +6,42 @@ function hide(id) {
 	$(id).css("display", "none");
 }
 
+function showDashboard() {
+	show('#dashboard'); 
+	hide('#your-blogs'); 
+	hide('#friends')
+}
+
+function showYourBlog() {
+	hide('#dashboard'); 
+	show('#your-blogs'); 
+	hide('#friends')
+}
+
+function showFriends() {
+	hide('#dashboard'); 
+	hide('#your-blogs'); 
+	show('#friends')
+}
+
+function showWriteBlog() {
+	show('#write-blog'); 
+	hide('#search-blog'); 
+	hide('#user-search')
+}
+
+function showSearchBlog() {
+	hide('#write-blog'); 
+	show('#search-blog'); 
+	hide('#user-search')
+}
+
+function showUserSearch() {
+	hide('#write-blog'); 
+	hide('#search-blog'); 
+	show('#user-search')
+}
+
 function controlYourBlog() {
 	var app = angular.module('table_module', []);
 	app.controller('your_blog_controller', function($scope) {
