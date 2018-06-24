d4956a50dd6913103e28390660d4ec4c047f94de
piyush <piyush2k13@gmail.com>
2018-06-24 21:09:26 +0530

    sync

--- backbone/public/js/app.marionette.js
+++ backbone/public/js/app.marionette.js
@@ -1,9 +1,24 @@
 var View = Backbone.Marionette.View.extend({
-    template: '#template-layout'
+    template: '#template-layout',
+    ui: {
+        save: '.save-btn',
+        close: '.close-btn'
+    },
+    events: {
+        'click @ui.save': 'handleSave'
+    },
+    triggers: {
+        'click @ui.close': 'close:view'
+    },
+    handleSave: function() {
+        console.log('Calling handleSave()');
+    },
+    onCloseView: function() {
+        console.log('Calling closeView event');
+    }
 });
 var App = Backbone.Marionette.Application.extend({
     region: '#app',
-
     onStart() {
         this.showView(new View());
     }
